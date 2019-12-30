6099277b29574f2c33bc81782259a381607e7a32
3a513120 <57247605+3a513120@users.noreply.github.com>
2019-12-31 04:13:26 +0800

    Add files via upload

---
+++ no11/src/main/java/com/example/no11/no11.java
@@ -0,0 +1,10 @@
+package com.example.no11;
+
+import java.util.Scanner;
+
+public class no11 {
+    public static void main(String[] args) {
+        PrizeNumber prizeNumber = new PrizeNumber("7_8.txt");
+        prizeNumber.Judge();
+    }
+}
